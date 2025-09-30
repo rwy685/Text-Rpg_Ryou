276b1a4a97e90c1de20f0c3bd679d210492f7512
rwy685 <rwy685@gmail.com>
2025-09-30 17:24:45 +0900

    step 2

--- Text_RPG/Text_RPG/Program.cs
+++ Text_RPG/Text_RPG/Program.cs
@@ -12,44 +12,94 @@ namespace TextRPG
 {
     public class Intro
     {
-        public void intromessage()
+        public int intromessage()
         {
+
             while (true)
             {
-                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.\n이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n 1. 상태보기 \n 2. 인벤토리");
+                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.\n" +
+                                  "이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n " +
+                                  "1. 상태보기 \n 2. 인벤토리");
                 Console.WriteLine("원하시는 행동을 입력해주세요. >>");
-                
+
                 int input = int.Parse(Console.ReadLine());
+
                 switch (input)
                 {
                     case 1:
-                        Console.WriteLine("상태보기를 선택했습니다.");
-                        break;
+                        Console.Clear();
+                        Console.WriteLine("상태보기를 선택했습니다.\n");
+                        return 1;
                     case 2:
-                        Console.WriteLine("인벤토리를 선택했습니다.");
+ 
+                        Console.Clear();
+                        Console.WriteLine("인벤토리를 선택했습니다.\n");
                         break;
-
                     default:
                         Console.WriteLine("잘못된 입력입니다.");
                         Console.Clear();
                         break;
                 }
             }
+
+        }
+
+    }
+    public class PlayerInfo
+    {
+        public int level = 10;
+        public string name = "Carl";
+        public string job = "Warrior";
+        public int attack = 5;
+        public int defense = 2;
+        public int hp = 100;
+        public int gold = 1000;
+
+        public void DisplayPinfo()
+        {
+            Console.WriteLine($"LV.{level}\n{name}({job})\n공격력: {attack}\n방어력:" +
+                              $"{defense}\n체력: {hp}\nGold: {gold}\n\n" +
+                              $"0. 나가기\n원하시는 행동을 입력해주세요.\n>>");
+            int input = int.Parse(Console.ReadLine());
+            if (input == 0)
+            {
+                
+            }
         }
         static void Main(string[] args)
         {
             Intro intro = new Intro();
-            intro.intromessage();
+            PlayerInfo playerinfo = new PlayerInfo();
+            bool isGameStart = true;
+   
+
+            while (isGameStart)
+            {
+                intro.intromessage();
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0: Console.Clear();
+                        break; // 다시 intro 실행
+
+                    case 1:
+                        playerinfo.DisplayPinfo();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("인벤 없음");
+                        break;
+
+                    default:
+                        isGameStart = false;
+                        break;
+                }
+
+            }
+
         }
     }
-    public class PlayerInfo
-    {
-        static int level = 10;
-        static string name = "Carl";
-        static string job = "Warrior";
-        static int attack = 5;
-        static int defense = 2;
-        static int hp = 100;
-        static int gold = 1000;
-    }
+
+    
 }
